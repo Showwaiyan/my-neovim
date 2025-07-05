abe465d70f39320cbd2ddb04898f5f5c7349b330
Show Wai Yan <showwaiyan555@gmail.com>
2025-07-06 00:06:40 +0800

    Add JS Debugger

---
+++ lua/plugins/debugger.lua
@@ -0,0 +1,67 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+		--setup
+		dapui.setup()
+
+		--languages debugger setup
+		-- JS
+		local js_debugger_path = vim.fn.stdpath("data")
+			.. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js"
+
+		dap.adapters["pwa-node"] = {
+			type = "server",
+			host = "localhost",
+			port = 8123,
+			executable = {
+				command = "node",
+				args = { js_debugger_path, "8123" },
+			},
+		}
+		dap.configurations.javascript = {
+			{
+				type = "pwa-node",
+				request = "launch",
+				name = "Launch file",
+				program = "${file}",
+				cwd = "${workspaceFolder}",
+				runtimeExecutable = "node",
+				console = "integratedTerminal",
+				skipFiles = { "<node_internals>/**" },
+			},
+		}
+
+		-- ui
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		-- debug
+		vim.keymap.set("n", "<Leader>dt", function()
+			dap.toggle_breakpoint()
+		end)
+		vim.keymap.set("n", "<Leader>dc", function()
+			dap.continue()
+		end)
+
+		-- DAP signs
+		vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "DiagnosticError" })
+		vim.fn.sign_define("DapStopped", { text = "🟦", texthl = "DiagnosticInfo", linehl = "Visual" })
+	end,
+}
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,18 +1,19 @@
-  return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-      -- Optional: "3rd/image.nvim",
-    },
-    lazy = false,
-    opts = {
-      -- Add any Neo-tree specific configuration here
-    },
-    config = function()
-      --NeoTree
-      vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal right<CR>', {})
-    end
-  }
+return {
+  --neo tree
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+		-- Optional: "3rd/image.nvim",
+	},
+	lazy = false,
+	opts = {
+		-- Add any Neo-tree specific configuration here
+	},
+	config = function()
+		--NeoTree
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal right<CR>", {})
+	end,
+}
