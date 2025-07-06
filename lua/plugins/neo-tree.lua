776c25401c01be7a9d095fd2a8086e172db5f23f
Show Wai Yan <showwaiyan555@gmail.com>
2025-07-06 16:29:59 +0800

    Add icons plugins

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,19 +1,19 @@
 return {
   --neo tree
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-		-- Optional: "3rd/image.nvim",
-	},
-	lazy = false,
-	opts = {
-		-- Add any Neo-tree specific configuration here
-	},
-	config = function()
-		--NeoTree
-		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal right<CR>", {})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+    -- Optional: "3rd/image.nvim",
+  },
+  lazy = false,
+  opts = {
+    -- Add any Neo-tree specific configuration here
+  },
+  config = function()
+    --NeoTree
+    vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal right<CR>", {})
+  end,
 }
