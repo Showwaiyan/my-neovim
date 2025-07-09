d1c0bc65da9fc544121af98b6d39d88d3eee12ad
Show Wai Yan <showwaiyan555@gmail.com>
2025-07-09 15:08:09 +0800

    Config attached debug server for node js

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,89 +1,89 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio",
-    "mfussenegger/nvim-dap-python",
-  },
-  config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"mfussenegger/nvim-dap-python",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
 
-    --setup
-    dapui.setup()
+		--setup
+		dapui.setup()
 
-    --languages debugger setup
-    -- JS
-    local js_debugger_path = vim.fn.stdpath("data")
-        .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js"
+		--languages debugger setup
+		-- JS
+		local js_debugger_path = vim.fn.stdpath("data")
+			.. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js"
 
-    dap.adapters["pwa-node"] = {
-      type = "server",
-      host = "localhost",
-      port = 8123,
-      executable = {
-        command = "node",
-        args = { js_debugger_path, "8123" },
-      },
-    }
-    dap.configurations.javascript = {
-      {
-        type = "pwa-node",
-        request = "launch",
-        name = "Launch file",
-        program = "${file}",
-        cwd = "${workspaceFolder}",
-        runtimeExecutable = "node",
-        console = "integratedTerminal",
-        skipFiles = { "<node_internals>/**" },
-      },
-    }
+		dap.adapters["pwa-node"] = {
+			type = "server",
+			host = "localhost",
+			port = 8123,
+			executable = {
+				command = "node",
+				args = { js_debugger_path, "8123" },
+			},
+		}
+		dap.configurations.javascript = {
+			{
+				type = "pwa-node",
+				request = "attach",
+				name = "Launch file",
+				program = "${file}",
+				cwd = "${workspaceFolder}",
+				runtimeExecutable = "node",
+				console = "integratedTerminal",
+				skipFiles = { "<node_internals>/**" },
+			},
+		}
 
-    --Python
-    local debugpy_path = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python"
-    dap.adapters.python = {
-      type = "executable",
-      command = debugpy_path,
-      args = { "-m", "debugpy.adapter" },
-    }
+		--Python
+		local debugpy_path = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python"
+		dap.adapters.python = {
+			type = "executable",
+			command = debugpy_path,
+			args = { "-m", "debugpy.adapter" },
+		}
 
-    dap.configurations.python = {
-      {
-        type = "python",
-        request = "launch",
-        name = "Launch file",
-        program = "${file}",
-        cwd = "${workspaceFolder}",
-      },
-    }
+		dap.configurations.python = {
+			{
+				type = "python",
+				request = "launch",
+				name = "Launch file",
+				program = "${file}",
+				cwd = "${workspaceFolder}",
+			},
+		}
 
-    -- ui
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+		-- ui
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
 
-    -- debug
-    vim.keymap.set("n", "<Leader>dt", function()
-      dap.toggle_breakpoint()
-    end)
-    vim.keymap.set("n", "<Leader>dc", function()
-      dap.continue()
-    end)
-    vim.keymap.set("n", "<Leader>do", dap.step_over, { desc = "DAP step over" })
-    vim.keymap.set("n", "<Leader>di", dap.step_into, { desc = "DAP step into" })
-    vim.keymap.set("n", "<Leader>du", dap.step_out, { desc = "DAP step out" })
+		-- debug
+		vim.keymap.set("n", "<Leader>dt", function()
+			dap.toggle_breakpoint()
+		end)
+		vim.keymap.set("n", "<Leader>dc", function()
+			dap.continue()
+		end)
+		vim.keymap.set("n", "<Leader>do", dap.step_over, { desc = "DAP step over" })
+		vim.keymap.set("n", "<Leader>di", dap.step_into, { desc = "DAP step into" })
+		vim.keymap.set("n", "<Leader>du", dap.step_out, { desc = "DAP step out" })
 
-    -- DAP signs
-    vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "DiagnosticError" })
-    vim.fn.sign_define("DapStopped", { text = "🟦", texthl = "DiagnosticInfo", linehl = "Visual" })
-  end,
+		-- DAP signs
+		vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "DiagnosticError" })
+		vim.fn.sign_define("DapStopped", { text = "🟦", texthl = "DiagnosticInfo", linehl = "Visual" })
+	end,
 }
