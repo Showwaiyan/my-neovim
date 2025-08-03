84decaff0e4cd04df9ae06ec0186d45eb48f5b72
Show Wai Yan <showwaiyan555@gmail.com>
2025-08-03 10:40:21 +0800

    Install auto-tag for react development

---
+++ lua/plugins/autotage.lua
@@ -0,0 +1,27 @@
+
+return {
+  "windwp/nvim-ts-autotag",
+  event = "InsertEnter", -- optional: lazy load
+  config = function()
+    require("nvim-ts-autotag").setup({
+      -- top-level keys, no 'opts' wrapper
+      enable_close = true,
+      enable_rename = true,
+      enable_close_on_slash = false,
+
+      -- Per filetype overrides
+      filetypes = {
+        "html",
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "svelte",
+        "vue",
+        "tsx",
+        "jsx",
+        "xml",
+      },
+    })
+  end,
+}
