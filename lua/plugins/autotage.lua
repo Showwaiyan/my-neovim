23c54d41fd57943905ef28f308c0fce1ed665f54
Show Wai Yan <showwaiyan555@gmail.com>
2025-08-11 10:55:21 +0800

    Add autotage for html and jsx

--- lua/plugins/autotage.lua
+++ lua/plugins/autotage.lua
@@ -1,27 +1,28 @@
-
 return {
-  "windwp/nvim-ts-autotag",
-  event = "InsertEnter", -- optional: lazy load
-  config = function()
-    require("nvim-ts-autotag").setup({
-      -- top-level keys, no 'opts' wrapper
-      enable_close = true,
-      enable_rename = true,
-      enable_close_on_slash = false,
+	"windwp/nvim-ts-autotag",
+	event = "InsertEnter", -- optional: lazy load
+	config = function()
+		require("nvim-ts-autotag").setup({
+			-- top-level keys, no 'opts' wrapper
+			opts = {
+				enable_close = true,
+				enable_rename = true,
+				enable_close_on_slash = false,
+			},
 
-      -- Per filetype overrides
-      filetypes = {
-        "html",
-        "javascript",
-        "javascriptreact",
-        "typescript",
-        "typescriptreact",
-        "svelte",
-        "vue",
-        "tsx",
-        "jsx",
-        "xml",
-      },
-    })
-  end,
+			-- Per filetype overrides
+			filetypes = {
+				"html",
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact",
+				"svelte",
+				"vue",
+				"tsx",
+				"jsx",
+				"xml",
+			},
+		})
+	end,
 }
