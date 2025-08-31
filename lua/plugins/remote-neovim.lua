aedbd56d4ee6aaa6b4219b2e7383f387060a7e3d
Show Wai Yan <showwaiyan555@gmail.com>
2025-08-31 10:30:08 +0800

    Add plugin for ssh development

---
+++ lua/plugins/remote-neovim.lua
@@ -0,0 +1,15 @@
+local utils = require("remote-nvim.utils")
+local constants = require("remote-nvim.constants")
+
+return {
+	"amitds1997/remote-nvim.nvim",
+	version = "*", -- Pin to GitHub releases
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- For standard functions
+		"MunifTanjim/nui.nvim", -- To build the plugin UI
+		"nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+	},
+	config = function()
+		require("remote-nvim").setup()
+	end,
+}
