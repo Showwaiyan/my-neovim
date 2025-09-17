1e93835348e272f4214440176533534ea5f88440
Show Wai Yan <showwaiyan555@gmail.com>
2025-09-17 16:29:22 +0800

    Add end point plugin

---
+++ lua/plugins/end-point.lua
@@ -0,0 +1,12 @@
+return {
+  "zerochae/endpoint.nvim",
+  dependencies = {
+    -- Choose one or more pickers (all optional):
+    "nvim-telescope/telescope.nvim", -- For telescope picker
+    -- vim.ui.select picker works without dependencies
+  },
+  cmd = { "Endpoint" },
+  config = function()
+    require("endpoint").setup()
+  end,
+}
