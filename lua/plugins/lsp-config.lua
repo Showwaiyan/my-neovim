a746490b3aeb579f4eb62e5508cdfd30b7fbf73c
Show Wai Yan <showwaiyan555@gmail.com>
2025-09-21 23:48:40 +0800

    Format code

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,156 +1,153 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    },
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    -- Trigger signature popup manually
-    "ray-x/lsp_signature.nvim",
-    event = "VeryLazy",
-    opts = {
-      bind = true,
-      handler_opts = {
-        border = "rounded",
-      },
-      hint_enable = false,  -- disable virtual text hint
-      floating_window = false, -- don't show popup automatically
-    },
-    config = function(_, opts)
-      local signature = require("lsp_signature")
-      signature.setup(opts)
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		-- Trigger signature popup manually
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		opts = {
+			bind = true,
+			handler_opts = {
+				border = "rounded",
+			},
+			hint_enable = false, -- disable virtual text hint
+			floating_window = false, -- don't show popup automatically
+		},
+		config = function(_, opts)
+			local signature = require("lsp_signature")
+			signature.setup(opts)
 
-      -- Optional: map a key to trigger signature help manually
-      vim.keymap.set("i", "<C-s>", function()
-        signature.toggle_float_win()
-      end, { desc = "LSP Signature Help" })
-    end,
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "stylua",
-          "html",
-          "cssls",
-          "ts_ls",
-          "emmet_ls",
-          "csharp_ls",
-          "jsonls",
-          "pylsp",
-          "jdtls",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      -- local lspconfig = require("lspconfig")
-      vim.lsp.config.lua_ls = { capabilities = capabilities }
-      vim.lsp.config.ts_ls = { capabilities = capabilities }
-      vim.lsp.config.html = { capabilities = capabilities }
-      vim.lsp.config.cssls = { capabilities = capabilities }
-      vim.lsp.config.emmet_ls = {
-        capabilities = capabilities,
-        filetypes = { "html", "css", "scss", "javascriptreact", "typescriptreact" },
-      }
-      vim.lsp.config.csharp_ls = { capabilities = capabilities }
-      vim.lsp.config.jsonls = { capabilities = capabilities }
-      vim.lsp.config.pylsp = { capabilities = capabilities }
-      vim.lsp.config.eslint = {
-        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-        settings = {
-          -- Example: auto-fix on save
-          format = { enable = true },
-        },
-        on_attach = function(client, bufnr)
-          -- optional: run ESLint fix on save
-          -- 	vim.api.nvim_create_autocmd("BufWritePre", {
-          -- 		buffer = bufnr,
-          -- 		command = "EslintFixAll",
-          -- 	})
-        end,
-      }
-      vim.lsp.config.jdtls = { capabilities = capabilities }
+			-- Optional: map a key to trigger signature help manually
+			vim.keymap.set("i", "<C-s>", function()
+				signature.toggle_float_win()
+			end, { desc = "LSP Signature Help" })
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"stylua",
+					"html",
+					"cssls",
+					"ts_ls",
+					"emmet_ls",
+					"csharp_ls",
+					"jsonls",
+					"pylsp",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			vim.lsp.config.lua_ls = { capabilities = capabilities }
+			vim.lsp.config.ts_ls = { capabilities = capabilities }
+			vim.lsp.config.html = { capabilities = capabilities }
+			vim.lsp.config.cssls = { capabilities = capabilities }
+			vim.lsp.config.emmet_ls = {
+				capabilities = capabilities,
+				filetypes = { "html", "css", "scss", "javascriptreact", "typescriptreact" },
+			}
+			vim.lsp.config.csharp_ls = { capabilities = capabilities }
+			vim.lsp.config.jsonls = { capabilities = capabilities }
+			vim.lsp.config.pylsp = { capabilities = capabilities }
+			vim.lsp.config.eslint = {
+				filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+				settings = {
+					-- Example: auto-fix on save
+					format = { enable = true },
+				},
+				on_attach = function(client, bufnr)
+					-- optional: run ESLint fix on save
+					-- 	vim.api.nvim_create_autocmd("BufWritePre", {
+					-- 		buffer = bufnr,
+					-- 		command = "EslintFixAll",
+					-- 	})
+				end,
+			}
 
-      -- Go to definition
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      -- Go to declaration
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-      -- Hover docs
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      -- Go to implementation
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-      -- Go to type definition
-      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, {})
-      -- Rename symbol
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-      -- Code action
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      -- Show references
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-      -- Format buffer
-      vim.keymap.set("n", "<leader>f", function()
-        vim.lsp.buf.format({ async = true })
-      end, {})
+			-- Go to definition
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			-- Go to declaration
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			-- Hover docs
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			-- Go to implementation
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			-- Go to type definition
+			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, {})
+			-- Rename symbol
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+			-- Code action
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			-- Show references
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			-- Format buffer
+			vim.keymap.set("n", "<leader>f", function()
+				vim.lsp.buf.format({ async = true })
+			end, {})
 
-      -- Auto-show floating diagnostic on hover
-      vim.cmd([[autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })]])
+			-- Auto-show floating diagnostic on hover
+			vim.cmd([[autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })]])
 
-      -- Auto-show floating diagnostic on hover
-      vim.cmd([[autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })]])
+			-- Auto-show floating diagnostic on hover
+			vim.cmd([[autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })]])
 
-      -- Optional: wrap and format diagnostic config
-      vim.diagnostic.config({
-        virtual_text = false, -- turn off inline text
-        float = {
-          border = "rounded",
-          source = "always",
-          max_width = 80,
-          wrap = true,
-        },
-        severity_sort = true,
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = " ",
-            [vim.diagnostic.severity.WARN] = " ",
-            [vim.diagnostic.severity.INFO] = "󰋼 ",
-            [vim.diagnostic.severity.HINT] = "󰌵 ",
-          },
-          numhl = {
-            [vim.diagnostic.severity.ERROR] = "",
-            [vim.diagnostic.severity.WARN] = "",
-            [vim.diagnostic.severity.HINT] = "",
-            [vim.diagnostic.severity.INFO] = "",
-          },
-        },
-        underline = true,
-        update_in_insert = false,
-      })
+			-- Optional: wrap and format diagnostic config
+			vim.diagnostic.config({
+				virtual_text = false, -- turn off inline text
+				float = {
+					border = "rounded",
+					source = "always",
+					max_width = 80,
+					wrap = true,
+				},
+				severity_sort = true,
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = " ",
+						[vim.diagnostic.severity.WARN] = " ",
+						[vim.diagnostic.severity.INFO] = "󰋼 ",
+						[vim.diagnostic.severity.HINT] = "󰌵 ",
+					},
+					numhl = {
+						[vim.diagnostic.severity.ERROR] = "",
+						[vim.diagnostic.severity.WARN] = "",
+						[vim.diagnostic.severity.HINT] = "",
+						[vim.diagnostic.severity.INFO] = "",
+					},
+				},
+				underline = true,
+				update_in_insert = false,
+			})
 
-      -- Optional: Diagnostics keymaps
-      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
-    end,
-  },
+			-- Optional: Diagnostics keymaps
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+		end,
+	},
 }
