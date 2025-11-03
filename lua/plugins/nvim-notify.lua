aaa5c4c3147550141f817347fdfd5e956e212e0e
Show Wai Yan <showwaiyan555@gmail.com>
2025-11-03 11:04:52 +0800

    Make notify pop up to bottom right instead of top right

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,13 +1,14 @@
 return {
-  {
-    "rcarriga/nvim-notify",
-    config = function()
-      vim.notify = require("notify")
-      require("notify").setup({
-        background_colour = "#000000", -- optional
-        stages = "fade_in_slide_out",
-        timeout = 2000,
-      })
-    end,
-  },
+	{
+		"rcarriga/nvim-notify",
+		config = function()
+			vim.notify = require("notify")
+			require("notify").setup({
+				background_colour = "#000000", -- optional
+				stages = "fade_in_slide_out",
+				timeout = 2000,
+				top_down = false,
+			})
+		end,
+	},
 }
